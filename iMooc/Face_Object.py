b9c885df3e99bc80e872f179d62f491343653da0
Arszrc <Arszrc@gmail.com>
2017-01-03 22:09:40 +0800

    aaa

---
+++ iMooc/Face_Object.py
@@ -0,0 +1,48 @@
+class classss(object):
+    """docstring for classss"""
+
+    hobby = "Coding"
+
+    def __init__(self, name, age, weight):
+        self.name = name
+        self._age = age
+        self.__weight = weight
+
+    @classmethod
+    def get_hobby(cls):
+        return cls.hobby
+
+    @property
+    def gett_weight(self):
+        return self.__weight
+
+    def self_introduction(self):
+        print("I am your father")
+
+
+# 继承
+class Backendclassss(classss):
+
+    def __init__(self, name, age, weight, language):
+        self.language = language
+
+# 多态
+class Duotaiclassss(classss):
+
+    def __init__(self, name, age, weight, language):
+        super(Duotaiclassss, self).__init__(name, age, weight)
+        self.language = language
+
+    def self_introduction(self):
+        print('My name is %s and I use %s' % (self.name, self.language))
+
+def introduce(classss_instance):
+    if isinstance(classss_instance, classss):
+        classss_instance.self_introduction()
+
+if __name__ == '__main__':
+    classss_instance = classss('余天堂', '22', '60')
+    Duotai_classss_instance = Duotaiclassss('Jobs', '50', '70', 'Python')
+    introduce(classss_instance)
+    introduce(Duotai_classss_instance)
+        
---
+++ mongoDB.py
@@ -0,0 +1,57 @@
+from pymongo import MongoClient
+from datetime import datetime
+
+client = MongoClient()
+
+db = client.test
+
+result = db.restaurants.insert_one(
+    {
+        "address": {
+            "street": "2 Avenue",
+            "zipcode": "10075",
+            "building": "1480",
+            "coord": [-73.9557413, 40.7720266]
+        },
+        "borough": "Manhattan",
+        "cuisine": "Italian",
+        "grades": [
+            {
+                "date": datetime.strptime("2014-10-01", "%Y-%m-%d"),
+                "grade": "A",
+                "score": 11
+            },
+            {
+                "date": datetime.strptime("2014-01-16", "%Y-%m-%d"),
+                "grade": "B",
+                "score": 17
+            }
+        ],
+        "name": "Vella",
+        "restaurant_id": "41704620"
+    }
+)
+
+cursor = db.restaurants.find()
+for document in cursor:
+	print(document)
+
+cursorB = db.restaurants.find({"grades.grade": "B"})
+for document in cursorB:
+    print(document)
+
+print(db.restaurants.isCapped())
+
+# Capped Collection
+
+# client.test.createCollection('capped_collection', { capped: true, size: 4096, max:100 })
+# capped_col = client.capped_collection
+# capped_col.insert_one(
+#     {
+#         "address": {
+#             "street": "2 Avenue",
+#             "zipcode": "10075",
+#             "building": "1480",
+#             "coord": [-73.9557413, 40.7720266]
+#         }
+#     })
