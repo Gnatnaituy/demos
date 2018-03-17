a9749e6eb2051eadbc7a835c5091118babc5cfd2
Hasaker <Arszrc@gmail.com>
2018-03-17 17:58:14 +0800

    就要考试了，，，，

---
+++ LanguageStudy/JavaStudy/src/book/thinkinginjava/chapter08/CatsAndDogs.java
@@ -0,0 +1,39 @@
+package book.thinkinginjava.chapter08;
+
+import java.util.*;
+import com.hasaker.tools.P;
+
+
+public class CatsAndDogs {
+    public static void main(String[] args) {
+        Vector cats = new Vector();
+        for (int i = 0; i < 7; i++)
+            cats.addElement(new Cat(i));
+        cats.addElement(new Dog(7));
+        for (int i = 0; i < cats.size(); i++)
+            ((Cat)cats.elementAt(i)).print();
+    }
+}
+
+
+class Cat {
+    private int catNumber;
+    Cat(int i) {
+        catNumber = i;
+    }
+    void print() {
+        P.rintln("Cat #" + catNumber);
+    }
+}
+
+
+class Dog {
+    private int dogNumber;
+    Dog(int i) {
+        dogNumber = i;
+    }
+    void print() {
+        P.rintln("Dog #" + dogNumber);
+    }
+}
+
---
+++ LanguageStudy/JavaStudy/src/book/thinkinginjava/chapter08/WorksAnyway.java
@@ -0,0 +1,41 @@
+package book.thinkinginjava.chapter08;
+
+import java.util.*;
+import com.hasaker.tools.P;
+
+
+public class WorksAnyway {
+    public static void main(String[] args) {
+        Vector mice = new Vector();
+        for (int i = 0; i < 3; i++)
+            mice.addElement(new Mouse(i));
+        for (int i = 0; i < mice.size(); i++) {
+            P.rintln("Free mouse : " + mice.elementAt(i));
+            MouseTrap.caughtYa(mice.elementAt(i));
+        }
+    }
+}
+
+
+class Mouse {
+    private int mouseNumber;
+    Mouse(int i) {
+        mouseNumber = i;
+    }
+    public String toString() {
+        return "This is mouse #" + mouseNumber;
+    }
+    void print(String msg) {
+        if(msg != null)
+            P.rintln(msg);
+        P.rintln("Mouse number " + mouseNumber);
+    }
+}
+
+
+class MouseTrap {
+    static void caughtYa(Object m) {
+        Mouse mouse = (Mouse)m;
+        mouse.print("caught one!");
+    }
+}
