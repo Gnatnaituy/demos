26aa821192afc96e69e33c3e2b53d68e3de5d600
Hasaker <hasaker@Hasakers-iMac.local>
2018-04-01 21:37:59 +0800

    还是喜欢Python

---
+++ LanguageStudy/JavaStudy/src/book/thinkinginjava/chapter08/GopherVector.java
@@ -0,0 +1,48 @@
+package book.thinkinginjava.chapter08;
+
+import java.util.Vector;
+
+class GopherVector {
+    private Vector v = new Vector();
+
+    public void addElement(Gopher m) {
+        v.addElement(m);
+    }
+
+    public Gopher elementAt(int index) {
+        return (Gopher)v.elementAt(index);
+    }
+
+    public int size() {return v.size();}
+
+    public static void main(String args[]) {
+        GopherVector gophers = new GopherVector();
+        for(int i = 0; i < 3; i++)
+            gophers.addElement(new Gopher(i));
+        for(int i = 0; i < 3; i++)
+            GopherTrap.caughtYa(gophers.elementAt(i));
+        System.out.println(gophers.size());
+    }
+
+}
+
+
+class Gopher {
+    private int gopherNumber;
+
+    Gopher(int i) {
+        gopherNumber = i;
+    }
+
+    void print(String msg) {
+        if(msg != null) System.out.println(msg);
+        System.out.println("Gopher Number " + gopherNumber);
+    }
+}
+
+
+class GopherTrap {
+    static void caughtYa(Gopher m) {
+        m.print("Caught one !");
+    }
+}
