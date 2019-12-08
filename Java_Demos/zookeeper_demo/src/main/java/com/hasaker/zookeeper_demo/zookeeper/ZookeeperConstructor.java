3692f93c8c013d68e8afe9bd01b577382e63b749
Hasaker <1758267482@qq.com>
2019-12-08 21:13:16 +0800

    Clear

--- Java_Demos/zookeeper_demo/src/main/java/com/hasaker/zookeeper_demo/zookeeper/ZookeeperConstructor.java
+++ Java_Demos/zookeeper_demo/src/main/java/com/hasaker/zookeeper_demo/zookeeper/ZookeeperConstructor.java
@@ -3,6 +3,7 @@ package com.hasaker.zookeeper_demo.zookeeper;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.zookeeper.*;
 
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
 /**
@@ -14,17 +15,26 @@ import java.util.concurrent.CountDownLatch;
 public class ZookeeperConstructor implements Watcher {
 
     private static CountDownLatch connectedSemaphore = new CountDownLatch(1);
+    private static ZooKeeper zooKeeper = null;
 
     @Override
     public void process(WatchedEvent watchedEvent) {
         log.info("Receive watched event: {}", watchedEvent);
         if (Event.KeeperState.SyncConnected == watchedEvent.getState()) {
-            connectedSemaphore.countDown();
+            if (Event.EventType.None == watchedEvent.getType() && null == watchedEvent.getPath()) {
+                connectedSemaphore.countDown();
+            } else if (watchedEvent.getType() == Event.EventType.NodeChildrenChanged) {
+                try {
+                    log.info("ReGet child: {}", zooKeeper.getChildren(watchedEvent.getPath(), true));
+                } catch (Exception e) {
+                    log.error("{}", e.toString());
+                }
+            }
         }
     }
 
     public static void main(String[] args) throws Exception {
-        ZooKeeper zooKeeper = new ZooKeeper("127.0.0.1:2181", 5000, new ZookeeperConstructor());
+        zooKeeper = new ZooKeeper("127.0.0.1:2181", 5000, new ZookeeperConstructor());
         log.info("Zookeeper stat: {}", zooKeeper.getState());
 
         try {
@@ -47,8 +57,21 @@ public class ZookeeperConstructor implements Watcher {
 //        log.info("Success create ZNode: {}", demoNode);
 
         // Create ZNode with asynchronous API
-        zooKeeper.create("/demo_node_async-", "".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL,
-                new DemoStringCallback(), "I am the context");
+//        zooKeeper.create("/demo_node_async-", "".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL,
+//                new DemoStringCallback(), "I am the context");
+
+        // Get children node list by synchronize API
+        List<String> children = zooKeeper.getChildren("/demo", true);
+        log.info("Children for /demo: {}", children);
+
+        for (int i = 4; i < 10; i++) {
+            zooKeeper.create("/demo/child_" + i, "".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        }
+
+        // Get children node list by asynchronous API
+
+
+
 
         Thread.sleep(Integer.MAX_VALUE);
     }
