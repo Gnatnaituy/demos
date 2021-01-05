cd57743afe586536a8b1cbe6bf10ddf4729f67aa
Ravooo <1758267482@qq.com>
2021-01-05 23:08:26 +0800

    reflect demo

--- java/foundation/src/main/java/base/ReflectDemo.java
+++ java/foundation/src/main/java/base/ReflectDemo.java
@@ -13,36 +13,42 @@ public class ReflectDemo {
     private static final String TAG = "Reflection";
 
     public static void main(String[] args) throws Exception {
-//        getClassMethods();
+        getClassMethods();
         testReflection();
     }
 
     public static void getClassMethods() throws ClassNotFoundException {
-        // 1.通过对象实例获取对应Class对象Object.getClass()--对于基本类型无法使用这种方法
-        System.out.println();
+        System.out.println("1.通过对象实例获取对应Class对象Object.getClass(), 基本类型不能使用这种方法");
         System.out.println("foo".getClass());
-        System.out.println(byte[].class);
-        System.out.println(HashSet.class);
-
-        // 2.通过类的类型获取Class对象,基本类型同样可以使用这种方法
+        System.out.println(Integer.valueOf(100).getClass());
+        System.out.println(Long.valueOf(100).getClass());
         System.out.println();
+
+        System.out.println("2.通过类的类型获取Class对象,基本类型同样可以使用这种方法");
         System.out.println(boolean.class);
-        System.out.println(int.class);
+        System.out.println(HashMap.class);
         System.out.println(double[][][][].class);
-
-        // 3.通过类的全限定名获取Class对象， 基本类型无法使用此方法
         System.out.println();
+
+        System.out.println("3.通过类的全限定名获取Class对象，基本类型无法使用此方法");
         System.out.println(Class.forName("java.lang.String")); //通过Class.forName()方法加载的类，采用的是系统类加载器对于数组比较特殊
         System.out.println(Class.forName("[D"));    //相当于double[].class
         System.out.println(Class.forName("[[Ljava.lang.String;"));   //相当于String[][].class
-
-        // 5.基本类型和void 类型的包装类可以使用TYPE字段获取
         System.out.println();
+
+        System.out.println("4.通过类加载器xxxClassLoader.loadClass()传入类路径获取, 基本类型无法使用此方法");
+        System.out.println(ClassLoader.getSystemClassLoader().loadClass("java.lang.String"));
+        System.out.println();
+
+        System.out.println("5.基本类型和void类型的包装类可以使用TYPE字段获取");
         System.out.println(Double.TYPE);
         System.out.println(Void.TYPE);
+        System.out.println();
 
         // 6.另外还有一些反射方法可以获取Class对象，但前提是你已经获取了一个Class对象
         // Class.getSuperclass() --> 获得给定类的父类Class
+
+        System.out.println();
     }
 
     public static void testReflection() {
@@ -50,6 +56,8 @@ public class ReflectDemo {
 
         // 获取类名
         System.out.println(TAG + " Class : " + c.getCanonicalName());
+        System.out.println(TAG + " Class : " + c.getSimpleName());
+        System.out.println(TAG + " Class : " + c.getSuperclass());
 
         // 获取类限定符
         System.out.println(TAG + " Modifiers : " + Modifier.toString(c.getModifiers()));
@@ -79,7 +87,7 @@ public class ReflectDemo {
         }
 
         //获取类继承数上的所有父类
-        List<Class> l = new ArrayList<>();
+        List<Class<?>> l = new ArrayList<>();
         printAncestor(c, l);
         if (l.size() != 0) {
             StringBuilder inheritance = new StringBuilder(" Inheritance Path : ");
@@ -106,7 +114,7 @@ public class ReflectDemo {
         }
     }
 
-    private static void printAncestor(Class<?> c, List<Class> l) {
+    private static void printAncestor(Class<?> c, List<Class<?>> l) {
         Class<?> ancestor = c.getSuperclass();
         if (ancestor != null) {
             l.add(ancestor);
