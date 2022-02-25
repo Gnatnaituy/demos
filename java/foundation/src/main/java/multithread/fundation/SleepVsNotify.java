fb42e1ec201d4b722ba96dda60ce39de1f88f9e1
Ravooo <1758267482@qq.com>
2022-02-25 22:07:08 +0800

    Thread.sleep() vs Object.wait()

--- java/foundation/src/main/java/multithread/fundation/SleepVsNotify.java
+++ java/foundation/src/main/java/multithread/fundation/SleepVsNotify.java
@@ -7,35 +7,10 @@ import lombok.extern.slf4j.Slf4j;
 public class SleepVsNotify {
 
     @AllArgsConstructor
-    static class SleepThread extends Thread {
+    static class SleepWaitThread extends Thread {
         private final String name;
-        private final long timeout;
-        private final Thread target;
-
-        @Override
-        public void run() {
-            log.info("{} started...", name);
-            try {
-                log.info("{} sleep {}ms start...", name, timeout);
-                Thread.sleep(timeout);
-            } catch (InterruptedException e) {
-                log.error("{} interrupted in sleep", name);
-                return;
-            }
-
-            if (target != null) {
-                target.interrupt();
-            }
-
-            log.info("{} finished", name);
-        }
-    }
-
-    @AllArgsConstructor
-    static class WaitThread extends Thread {
-        private final String name;
-        private final long sleepTimeout;
-        private final long waitTimeout;
+        private final long sleepSec;
+        private final long waitSec;
         private final Thread target;
         private final String lock;
 
@@ -44,10 +19,10 @@ public class SleepVsNotify {
             log.info("{} started...", name);
             synchronized (lock) {
                 try {
-                    log.info("{} get lock on {}, start sleep for {}ms...", name, lock, sleepTimeout);
-                    Thread.sleep(sleepTimeout);
-                    log.info("{} wakeup, start wait for {}ms", name, waitTimeout);
-                    lock.wait(waitTimeout);
+                    log.info("{} get lock on {}, start sleep for {}s...", name, lock, sleepSec);
+                    Thread.sleep(sleepSec * 1000);
+                    log.info("{} wakeup, start wait for {}s", name, waitSec);
+                    lock.wait(waitSec * 1000);
                 } catch (InterruptedException e) {
                     log.error("{} interrupted while waiting", name);
                     return;
@@ -62,22 +37,13 @@ public class SleepVsNotify {
         }
     }
 
-    public void testSleep() {
-        Thread thread1 = new SleepThread("Thread-1", 10000, null);
-        Thread thread2 = new SleepThread("Thread-2", 5000, thread1);
-
-        thread1.start();
-        thread2.start();
-    }
-
-    public void testWait() {
+    public void testSleepWait() {
         String lock1 = "Lock-1";
         String lock2 = "Lock-2";
-        String lock3 = "Lock-3";
 
-        WaitThread thread1 = new WaitThread("WaitThread-1", 10000, 10000, null, lock1);
-        WaitThread thread2 = new WaitThread("WaitThread-2", 5000, 5000, thread1, lock1);
-        WaitThread thread3 = new WaitThread("WaitThread-3", 20000, 20000,null, lock3);
+        SleepWaitThread thread1 = new SleepWaitThread("SleepWaitThread-1", 5, 5, null, lock1);
+        SleepWaitThread thread2 = new SleepWaitThread("SleepWaitThread-2", 2, 2, thread1, lock2);
+        SleepWaitThread thread3 = new SleepWaitThread("SleepWaitThread-3", 5, 5,null, lock2);
 
         thread1.start();
         thread2.start();
@@ -86,7 +52,6 @@ public class SleepVsNotify {
 
     public static void main(String[] args) {
         SleepVsNotify sleepVsNotify = new SleepVsNotify();
-        sleepVsNotify.testSleep();
-        sleepVsNotify.testWait();
+        sleepVsNotify.testSleepWait();
     }
 }
