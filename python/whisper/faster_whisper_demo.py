be0fd4d6e7c8f3c573cedfd72b7bfbacede966b2
Ravooo <1758267482@qq.com>
2023-07-11 21:54:04 +0800

    solve conflict

--- python/whisper/faster_whisper_demo.py
+++ python/whisper/faster_whisper_demo.py
@@ -1,19 +1,29 @@
 from faster_whisper import WhisperModel
+from datetime import datetime
+import psutil
+import os
 
-model_size = 'medium'
+FILE_PATH = '/root/Code/demos/python/whisper/audio/time_es/'
 
-# Run on GPU with FP16
-model = WhisperModel(model_size, device="cuda", compute_type="int8")
-# or run on GPU with INT8
-# model = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
-# or run on CPU with INT8
-# model = WhisperModel(model_size, device="cpu", compute_type="int8")
+def transcribe_file(file): 
+    file_name = FILE_PATH + file
+    start_time = datetime.now()
+    model = WhisperModel('medium', device="cpu", compute_type="float32")
+    print("File: %s" % file)
+    segments, info = model.transcribe(file_name, beam_size=5, language='es')
 
-file_name = '/root/Code/demos/python/whisper/audio/MockingBird.flac'
+    mem_showed = False
+    for segment in segments:
+        if (mem_showed == False):
+            print("Memory: %.4fGB" % (psutil.Process(os.getpid()).memory_info().rss / 1024 / 1024 / 1024))
+            mem_showed = True
+        # print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
 
-segments, info = model.transcribe(file_name, beam_size=5)
+    print("Time: %ss" % (datetime.now() - start_time))
 
-print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
 
-for segment in segments:
-    print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
+if __name__ == "__main__":
+    for file in os.listdir(FILE_PATH):
+        if file.endswith("mp3"):
+            transcribe_file(file=file)
+            print('\n')
